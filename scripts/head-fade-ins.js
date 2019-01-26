5e08673f7015884632ae694bcc209acf23789ee9
ChChrisDesigns <ch.chris.designs@gmail.com>
2019-01-25 18:23:42 -0800

    js fade up for headlines

---
+++ scripts/head-fade-ins.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+
+	hideObjects();
+	checkObjectsVisibility();
+
+	$(window).scroll( function() {
+		hideObjects();
+		checkObjectsVisibility();
+	});
+
+	function hideObjects() {
+		$('#fadeInUp').css({
+			'opacity': 0,
+			'transform': 'translateY(100px)'
+		});
+	}
+
+	function checkObjectsVisibility() {
+		$('#fadeInUp').each( function(i) {
+			var objectTop = $(this).offset().top;
+			var windowBottom = $(window).scrollTop() + $(window).outerHeight();
+
+			if( windowBottom > objectTop - 100){
+				$(this).addClass('visible');
+			} else {
+				$(this).removeClass('visible');
+			}
+		});
+	}
+});
